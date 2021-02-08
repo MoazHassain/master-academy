5952d960c79538d07f9fe4e4c562d8f5da840e42
MoazHassain <moazhassain@gmail.com>
2021-02-08 12:15:36 +0600

    lecture10-slice

--- master-academy/lecture10.go
+++ master-academy/lecture10.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+	// array
 	var student [3]string
-
 	student[0] = "moaz"
 	student[1] = "miraz"
 	student[2] = "mitul"
@@ -12,4 +15,22 @@ func main() {
 	fmt.Println(student)
 	fmt.Println(len(student))
 	fmt.Println(student[2])
+	// short hand way array declaration
+	teacher := [2]string{"mostain", "mubir"}
+	fmt.Println(teacher)
+
+	// slice
+	group01 := student[0:1]
+	fmt.Println(group01)
+	// declaring elements on slice
+	var fruit []string
+	fruit = append(fruit, "apple", "grape")
+	fmt.Println(fruit)
+	fmt.Printf("%T", fruit) //show the data type
+
+	// reflect: to see exactly what type of data it is
+	a := reflect.TypeOf(student).Kind().String()
+	fmt.Println(a)
+	b := reflect.TypeOf(fruit).Kind().String()
+	fmt.Println(b)
 }
